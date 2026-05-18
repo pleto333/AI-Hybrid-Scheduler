278dff170587bb2489824838516fb2ca66700005
oowgnus <sungwoo9027@naver.com>
2026-05-18 17:08:52 +0900

    스케줄링 알고리즘 구현 및 차선책 알고리즘 도입, 스케줄링 알고리즘 구현시 계산식도 구현되어있음

--- src/kernel/hardware.py
+++ src/kernel/hardware.py
@@ -1,8 +1,9 @@
 class Core:
-    def __init__(self, core_id, core_type, multiplier):
+    def __init__(self, core_id, core_type, multiplier, power_consumption):
         self.core_id = core_id
         self.core_type = core_type  # "P" or "E"
         self.multiplier = multiplier
+        self.power_consumption = power_consumption # 틱당 전력 소모량
         self.current_process = None
 
     def is_busy(self):
@@ -10,8 +11,10 @@ class Core:
 
 class PCore(Core):
     def __init__(self, core_id):
-        super().__init__(core_id, "P", 2.0)
+        # P코어: 속도 2배, 전력소모 3.0
+        super().__init__(core_id, "P", 2.0, 3.0)
 
 class ECore(Core):
     def __init__(self, core_id):
-        super().__init__(core_id, "E", 1.0)
+        # E코어: 속도 1배, 전력소모 1.0
+        super().__init__(core_id, "E", 1.0, 1.0)
